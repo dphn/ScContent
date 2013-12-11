a29b143ff4b9d91054720b90a9f1f7c1a23c0c5a
dphn <work.dolphin@gmail.com>
2013-12-12 00:57:40 +0200

    Changed greeting.

--- src/ScContent/Entity/Front/Content.php
+++ src/ScContent/Entity/Front/Content.php
@@ -7,12 +7,6 @@ use ScContent\Entity\AbstractContent,
 
 class Content extends AbstractContent implements FormatProviderInterface
 {
-    protected $type = 'category';
-
-    protected $title = 'Welcome!';
-
-    protected $description = 'Create a home page of your site. To do this, add an article or category and assign it the name "home". Do not forget to change its status to "published".';
-
     public function setType($type)
     {
         $this->type = $type;
