b218fad5ea699cd7bb01113aac3636285bf622d0
dphn <work.dolphin@gmail.com>
2014-02-14 23:07:15 +0200

    remove installation credentials

--- settings/installation.passwd.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'username' => '$2y$10$TW67teg/n9GWzdXHFvBmauPiU0usViQZ689/8IGhhxsVZ2Y45puqe',
-  'password' => '$2y$10$b/GamiHO8HrE7gL3bBlhtOjBajzNfSjUh/m9CbvTxIYc1KoLSjsFW',
-);
